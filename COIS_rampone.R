1e8847936d048ae9310c08bc15c83ad297bb1eb0
Diego <diego.rincon@wsu.edu>
2026-05-20 00:04:30 -0700

    forgot the code

--- COIS_rampone.R
+++ COIS_rampone.R
@@ -38,6 +38,34 @@ summer2EH <- function(DDs){
 }
 
 
+# Necessary functions and declaration to produce colors and align curves
+
+t_col <- function(color, percent = 50, name = NULL) {
+  #      color = color name
+  #    percent = % transparency
+  #       name = an optional name for the color
+  
+  ## Get RGB values for named color
+  rgb.val <- col2rgb(color)
+  
+  ## Make new color using input color as base and alpha set by transparency
+  t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3],
+               max = 255,
+               alpha = (100 - percent) * 255 / 100,
+               names = name)
+  
+  ## Save the color
+  invisible(t.col)
+}
+
+
+
+eggcol <- t_col("grey", percent = 50)
+crowcol <- t_col("brown", percent = 50)
+
+
+################################################################################
+
 DDs = seq(100, 2000)
 
 ################################################################################
